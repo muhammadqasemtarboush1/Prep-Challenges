661f0304940419bc87f8b5389dbe699780e21596
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-04-21 06:54:59 +0300

    Solve: Problem 3

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -56,7 +56,11 @@ const sumNums = (arr)=>{
 // Output: ['Python','Ruby','JS','C#']
 
 const reverseArray = (arr)=>{
-    // write your code here
+    let reversedArr= [];
+    for(let i=arr.length -1 ;i=>0;i--){
+        reversedArr.push(arr[i]);
+    }
+return reversedArr
 }
 // -------------------------------------------------------------------------------------------------------
 
